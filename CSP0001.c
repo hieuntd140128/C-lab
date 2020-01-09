deb74a3401b7770429d68df2e3ae34099b07ebf5
hieuntd140128 <59680355+hieuntd140128@users.noreply.github.com>
2020-01-09 23:09:40 +0700

    Create CSP0001.c

---
+++ CSP0001.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	//declare
+	char str[100];
+	int i,key;
+	do
+	{
+		// input 1 string
+		printf("\n nhap chuoi : ");
+		gets(str);
+		printf("dao chuoi  :");
+		// dao chuoi
+		for (i=strlen(str)-1;i>=0;i--)
+		{
+			if(str[i]==' ')
+			{
+				str[i]='\0';
+				printf("%s ",&str[i]+1);
+			}
+		}
+		printf("%s",str);
+		printf("\nnhap phim enter de tiep tuc");
+		key=getch();
+	}while(key!=27);
+	}
